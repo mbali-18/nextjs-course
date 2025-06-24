3fb275939f5dabf6cd968ca57bcb687ea85358ee
mbali-18 <faithfulservent18@gmail.com>
2025-06-24 16:40:18 +0200

    working with nested paths & routes

---
+++ pages/portfolio/index.js
@@ -0,0 +1,10 @@
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>The Portfolio Page</h1>
+      <p>This is the portfolio page content.</p>
+    </div>
+  );
+}
+
+export default PortfolioPage;
