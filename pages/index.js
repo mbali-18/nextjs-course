f953be033d3ea86e6d014da2d26216a3524917af
mbali-18 <faithfulservent18@gmail.com>
2025-06-24 16:28:08 +0200

    adding a first page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Home Page</h1>
+      <p>This is the main content of the home page.</p>
+    </div>
+  );
+}
+
+export default HomePage;
